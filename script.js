3d3343bd2dfecbae502eb9663d6f94d663ff3dcb
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-26 11:07:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+            let counterElement = document.getElementById('counter');
+            const incrementButton = document.getElementById('incrementBtn');
+
+            function handleIncrement() {
+                const currentValue = parseInt(counterElement.innerText);
+                alert(currentValue);
+                const incrementedValue = currentValue + 1;
+                counterElement.innerText = incrementedValue;
+            }
+
+            incrementButton.addEventListener('click', handleIncrement);
+        });
